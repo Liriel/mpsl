30ea62b17e902db9b97e9017b9501fb815ac3910
Starbuck <lassi@heisl.org>
2022-10-09 15:30:06 +0200

    udpate meta colo

--- publish.sh
+++ publish.sh
@@ -2,5 +2,6 @@
 pushd ClientApp
 npm run publish
 popd
-docker build -f Dockerfile.alpine-x64-slim -t liriel/mpsl .
-docker push liriel/mpsl
+docker build -f Dockerfile.alpine-x64-slim -t peach.heisl.org/mpsl .
+docker push peach.heisl.org/mpsl
+#ssh core@192.168.0.248 "cd mpsl && ./updateContainer.sh"
