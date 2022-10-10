505e346631e47ab48552b810918f2ca19b3b270f
Starbuck <lassi@heisl.org>
2022-10-10 14:59:50 +0200

    fixed service worker exceptions, media tag

--- publish.sh
+++ publish.sh
@@ -4,4 +4,4 @@ npm run publish
 popd
 docker build -f Dockerfile.alpine-x64-slim -t peach.heisl.org/mpsl .
 docker push peach.heisl.org/mpsl
-#ssh core@192.168.0.248 "cd mpsl && ./updateContainer.sh"
+ssh core@192.168.0.248 "cd mpsl && ./updateContainer.sh"
