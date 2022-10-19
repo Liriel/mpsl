b44a4f83746b6cd19ed57cac1252d386b2cb0a08
Starbuck <lassi@heisl.org>
2022-10-19 17:28:51 +0200

    publish script now updates manifest

--- publish.sh
+++ publish.sh
@@ -2,10 +2,10 @@
 pushd ClientApp
 npm run publish
 popd
-docker build -f Dockerfile.alpine-x64-slim -t peach.heisl.org/mpsl .
+docker build -f Dockerfile.alpine-x64-slim -t peach.heisl.org/mpsl-amd64 .
 docker push peach.heisl.org/mpsl-amd64
 docker build -f Dockerfile.bullseye-slim-arm64v8 -t peach.heisl.org/mpsl-arm64 .
 docker push peach.heisl.org/mpsl-arm64
 docker manifest create peach.heisl.org/mpsl --amend peach.heisl.org/mpsl-amd64 --amend peach.heisl.org/mpsl-arm64
-docker manifest push peach.heisl.org/mpsl
+docker manifest push --purge peach.heisl.org/mpsl
 ssh core@192.168.0.248 "cd mpsl && ./updateContainer.sh"
